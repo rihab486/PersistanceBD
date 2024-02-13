1dd022097e50cb74475cb55b06366b49f266e1ef
zoubaidachatti <71558413+zoubaidachatti@users.noreply.github.com>
2024-02-13 11:02:05 +0100

    datafixtures sqlite ,mysql ,relation

--- src/DataFixtures/BlogFixtures.php
+++ src/DataFixtures/BlogFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Address;
+use App\Entity\Article;
+use App\Entity\Category;
 use App\Entity\Profile;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -29,6 +31,7 @@ class BlogFixtures extends Fixture
                                      -> setCodePostal($faker -> codePostal())
                                      ->setCity($faker->city())
                                      ->setCountry($faker->country())
+                                     ->setCreatedAt($faker->dateTimeImmutable())
             ;
 
             // $profile= (new Profile())->setPicture($faker -> image())
@@ -42,12 +45,42 @@ class BlogFixtures extends Fixture
 
 
             $user->addAddress($address);
-          //  $user->setProfile($profile);
-            $users =$user;
+          //  $user->setUser($profile);
+            $users[] =$user;
             $manager->persist($user);
             $manager->persist($address);
 
 
+
+        }
+        $categories = [];
+        for ($i = 0; $i <10 ; $i++){
+            $category= (new Category())-> setName($faker->name())
+                                       ->setDescrption($faker->description(60))
+                                    ->setImageUrl($faker->image())
+                                    ->setCreatedAt($faker->dateTimeImmutable())
+
+            ;
+            $categories[]=$category;
+            $manager->persist($category);
+
+            
+
+        }
+       
+        for ($i = 0; $i <300 ; $i++){
+            $article= (new Article())-> setTitle($faker->title())
+                                     ->setContent($faker->text(5,10))
+                                     ->setImageUrl($faker->image())
+                                     ->setCreateAt($faker->dateTimeImmutable())
+                                     ->setAuthor($users[rand(0,count($users) -1)])
+                                     ->addCategory($categories[rand(0,count($categories) -1)]);
+
+            ;
+           
+            $manager->persist($article);
+
+            
 
         }
       
